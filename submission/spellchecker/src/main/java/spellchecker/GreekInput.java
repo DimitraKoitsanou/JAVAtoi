e635807065e1161887ce5d231dd258fb5259bd07
Dimitra Koitsanou <32490290+DimitraKoitsanou@users.noreply.github.com>
2017-11-17 22:54:19 +0200

    Add files via upload
    Prwto anebasma klashs ever. Klash GreekInput

---
+++ submission/spellchecker/src/main/java/spellchecker/GreekInput.java
@@ -0,0 +1,12 @@
+package spellchecker;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class GreekInput {
+	
+	 public static void main(String[]args)throws IOException{
+		 InputStreamReader reader = new InputStreamReader(System.in, "Cp737");
+		 BufferedReader br = new BufferedReader(reader);
+	 }
+}
---
+++ submission/spellchecker/src/test/java/spellchecker/GreekInputTest.java
@@ -0,0 +1,14 @@
+package spellchecker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreekInputTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
