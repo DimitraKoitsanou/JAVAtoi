2f22c1e2d66a7dd5f80995f84ba64eccd28fbfe6
unknown <vmpili@N2L7P126.neloi.aueb.gr>
2017-11-20 18:21:35 +0200

    add SplitWords class

---
+++ spellcheck/src/main/java/spellcheck/SplitWords.java
@@ -0,0 +1,23 @@
+package spellcheck;
+import java.util.LinkedList;
+
+public class SplitWords {
+	
+	private String text;
+	
+	public SplitWords(String text) {
+		this.text = text;
+	}
+
+	public  LinkedList<String> getUserWords() {
+
+		String [] userWords = text.replaceAll("\\p{P}", "").toLowerCase().split("\\s+");
+		LinkedList<String> userText = new LinkedList<String>();
+
+		for(String word : userWords){
+			userText.add(word);
+		}
+	    return userText;
+	}
+
+}
