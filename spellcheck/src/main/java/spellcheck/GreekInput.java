14e0177b75b165edf9fd6a8cd07e5dabf560bd6d
DimitraKoitsanou <dkoitsanou@yahoo.com>
2017-11-23 11:20:42 +0200

    handle UnsupportedEncodingxception & IOException

--- spellcheck/src/main/java/spellcheck/GreekInput.java
+++ spellcheck/src/main/java/spellcheck/GreekInput.java
@@ -3,17 +3,39 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
-public class GreekInput {
-	private String codepage;
+public class GreekInput { 
+	
+	private String codepage; 
 	
 	public GreekInput(String codepage){
 		this.codepage = codepage;
 	}
-	
-	public String readLine() throws IOException{
-		 Reader reader = new InputStreamReader(System.in, codepage );
-		 BufferedReader br = new BufferedReader(reader);
-		 return br.readLine();
+	public void setCodePage( String codepage) {
+		this.codepage =  codepage;
+	}
+	 public String getCodePage() {
+		 return codepage;
+	 }
+
+	public String readLine() {
+		
+		try {
+			Reader reader = new InputStreamReader(System.in, codepage );
+			BufferedReader br = new BufferedReader(reader);
+			return br.readLine();
+			} catch(UnsupportedEncodingException e) {
+				System.out.println (e.toString());
+				System.out.println(codepage + "δεν υποστηρίζεται!");
+				System.out.println("Τερματισμός προγράμματος!");
+				System.exit(0);
+				return "";
+			} catch (IOException e) {
+				System.out.println(e.toString());
+				return "";
+			}
+		
+			
 	 }
 }
