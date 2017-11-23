b21ee4eb768a0e1af974ee25d18ce8d834a7d184
mtoli <marialenato@hotmail.com>
2017-11-23 11:47:15 +0200

    handle UnsupportedEncodingException

--- spellcheck/src/main/java/spellcheck/GreekOutput.java
+++ spellcheck/src/main/java/spellcheck/GreekOutput.java
@@ -1,34 +1,30 @@
 package spellcheck;
 
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class GreekOutput {
 
+private String codepage;
 
+		public GreekOutput(String codepage) {
+		this.codepage = codepage;
+		}
 
-
-
-
-		public GreekOutput() {}
-
-		 public void printText(String text) {
+		 public void printLine(String text) {
 
 					try{
 
-					PrintWriter p = new PrintWriter(new OutputStreamWriter(System.out,"Cp737"),true);
+					PrintWriter p = new PrintWriter(new OutputStreamWriter(System.out,codepage),true);
 					p.println(text);
 
 					}
-					  catch (Exception e){
-						    if(e instanceof IOException){
-
-						    }
-						}
-
+					  catch (UnsupportedEncodingException e){
+						    System.out.println(e.toString());
+						    System.out.println(codepage + "δεν υποστιρίζεται");
+						    System.out.println("Τερματισμός προγράμματος");
+						    System.exit(0);
+				         }
+					}
 				}
-		   }
-
-
-
