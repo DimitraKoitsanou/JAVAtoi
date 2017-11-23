f5d31ddf9e9105997543d6eaeb553f0b690212bc
kmandeleni <t8160067@dias.aueb>
2017-11-23 14:15:14 +0200

    Add printNonExistedWords method

--- spellcheck/src/main/java/spellcheck/CheckWord.java
+++ spellcheck/src/main/java/spellcheck/CheckWord.java
@@ -1,26 +1,40 @@
 package spellcheck;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class CheckWord {
-	private LinkedList<String> dictionary;
-	private LinkedList<String> userText;
+	private HashSet<String> dictionary;
+	private LinkedList<String> wordsToCheck;
+	private String codepage;
 
-
-	public CheckWord(LinkedList<String> dictionary, LinkedList<String> userText) {
+	public CheckWord(HashSet<String> dictionary, LinkedList<String> wordsToCheck, String codepage) {
 
 		this.dictionary = dictionary;
-		this.userText = userText;
+		this.wordsToCheck = wordsToCheck;
+		this.codepage = codepage;
+	}
+	
+	public void setDictionary(HashSet<String> dictionary) {
+		this.dictionary = dictionary;
+	}
+	
+	public void setWordsToCheck(LinkedList<String> wordsToCheck) {
+		this.wordsToCheck = wordsToCheck;
 	}
 
-	public Boolean compareLists(LinkedList<String> dictionary, LinkedList<String> userText) {
-		Boolean exitCode = null;
-		for (String userWord : userText) {
-			if (dictionary.contains(userWord)) {
-				exitCode = true;
-			} else {
-				exitCode = false;
-			}
+	public void setCodepage(String codepage) {
+		this.codepage = codepage;
+	}
+	
+	public LinkedList<String> printNotExistedWords() {
+		LinkedList<String> nonExistedWords = new LinkedList<String>();
+		GreekOutput go = new GreekOutput(codepage);
+		
+		for(String temp: wordsToCheck)
+			if(! dictionary.contains(temp)) {
+				go.printLine("Η λέξη '" + temp + "' δεν υπάρχει στο λεξικό μου.");
+				nonExistedWords.add(temp);
 		}
-		return exitCode;
+		return nonExistedWords;
 	}
 }
